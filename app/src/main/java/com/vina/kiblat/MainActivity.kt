24a9a093bd8f053821db368332e3d8a623843779
Vina <pmobxinfosys@gmail.com>
2022-04-14 19:50:57 +0700

    Qiblat

---
+++ app/src/main/java/com/vina/kiblat/MainActivity.kt
@@ -0,0 +1,220 @@
+package com.vina.kiblat
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.*
+import android.location.Location
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
+import kotlin.math.roundToInt
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+        const val MARSHMALLOW = 23
+
+        const val ACCESS_FINE_LOCATION_REQ_CODE = 35
+
+        const val QIBLA_LATITUDE = 21.3891
+        const val QIBLA_LONGITUDE = 39.8579
+    }
+
+    var currentDegree: Float = 0f
+    var currentNeedleDegree: Float = 0f
+
+    lateinit var sensorManager: SensorManager
+    lateinit var sensor: Sensor
+    lateinit var userLocation: Location
+
+    lateinit var needleAnimation: RotateAnimation
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        needleAnimation = RotateAnimation(
+            currentNeedleDegree,
+            0f,
+            Animation.RELATIVE_TO_SELF,
+            .5f,
+            Animation.RELATIVE_TO_SELF,
+            .5f
+        )
+
+        initLocationPermissions()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == ACCESS_FINE_LOCATION_REQ_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initLocationListener()
+            }
+        }
+    }
+
+    private fun initLocationPermissions() {
+        if (Build.VERSION.SDK_INT >= MARSHMALLOW) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                initLocationListener()
+            } else {
+                requestPermission()
+            }
+        } else {
+            initLocationListener()
+        }
+    }
+
+    private fun initLocationListener() {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            Log.d(TAG, "User Location : Lat : ${it.latitude} Long : ${it.longitude}")
+            initQiblaDirection(it.latitude, it.longitude)
+        }
+        fusedLocationClient.lastLocation.addOnFailureListener {
+            Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.location_permission_title))
+                .setMessage(getString(R.string.location_permission_message))
+                .setPositiveButton("Grant") { dialog, _ ->
+                    dialog.dismiss()
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        ACCESS_FINE_LOCATION_REQ_CODE
+                    )
+                }
+                .setNegativeButton("Deny") { dialog, _ ->
+                    dialog.dismiss()
+                    handlePermissionDenied()
+                }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                ACCESS_FINE_LOCATION_REQ_CODE
+            )
+        }
+    }
+
+    private fun handlePermissionDenied() {
+
+    }
+
+    private fun initQiblaDirection(latitude: Double, longitude: Double) {
+
+        val iv_compasskaabah = findViewById<ImageView>(R.id.iv_compass)
+        val tv_qibla = findViewById<TextView>(R.id.tv_qibla)
+
+        userLocation = Location("User Location")
+        userLocation.latitude = latitude
+        userLocation.longitude = longitude
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION)
+        sensorManager.registerListener(object : SensorEventListener {
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onSensorChanged(sensorEvent: SensorEvent?) {
+                val degree: Float = sensorEvent?.values?.get(0)?.roundToInt()?.toFloat()!!
+                var head: Float = sensorEvent.values?.get(0)?.roundToInt()?.toFloat()!!
+
+                val destLocation = Location("Destination Location")
+                destLocation.latitude = QIBLA_LATITUDE
+                destLocation.longitude = QIBLA_LONGITUDE
+
+                var bearTo = userLocation.bearingTo(destLocation)
+
+                val geoField = GeomagneticField(
+                    userLocation.latitude.toFloat(),
+                    userLocation.longitude.toFloat(),
+                    userLocation.altitude.toFloat(),
+                    System.currentTimeMillis()
+                )
+
+                head -= geoField.declination
+
+                if (bearTo < 0) {
+                    bearTo += 360
+                }
+
+                var direction = bearTo - head
+
+                if (direction < 0) {
+                    direction += 360
+                }
+
+                tv_qibla.text = "$currentDegree"
+
+                Log.d(TAG, "Needle Degree : $currentNeedleDegree, Direction : $direction")
+
+                needleAnimation = RotateAnimation(
+                    currentNeedleDegree,
+                    direction,
+                    Animation.RELATIVE_TO_SELF,
+                    .5f,
+                    Animation.RELATIVE_TO_SELF,
+                    .5f
+                )
+                needleAnimation.fillAfter = true
+                needleAnimation.duration = 200
+                iv_compasskaabah.startAnimation(needleAnimation)
+
+                currentNeedleDegree = direction
+                currentDegree = degree
+
+            }
+        }, sensor, SensorManager.SENSOR_DELAY_GAME)
+    }
+}
---
+++ app/src/main/java/com/vina/kiblat/QiblaApp.kt
@@ -0,0 +1,11 @@
+package com.vina.kiblat
+
+import android.app.Application
+
+class QiblaApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
